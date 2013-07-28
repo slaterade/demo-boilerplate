1dde76d8eecc2cc4ce93ef5fd608a65b21ec583d
sudopunk <w.richard.slater@gmail.com>
2013-07-27 21:27:44 -0500

    added GLEW and test code

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,9 +1,146 @@
-// This main function was taken from the GLFW documentation
+// the source code below is kind of a hybrid between two tutorials
+// primarily this boilerplate will test glew and glfw and access to the shader pipeline
 // http://www.glfw.org
+// http://www.arcsynthesis.org/gltut
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <thread>
+#include <vector>
+#include <algorithm>
+
+GLuint CreateShader( GLenum eShaderType, const std::string& strShaderFile ) {
+	GLuint shader = glCreateShader( eShaderType );
+	const char* strFileData = strShaderFile.c_str();
+	glShaderSource( shader, 1, &strFileData, NULL );
+
+	glCompileShader( shader );
+
+	GLint status;
+	glGetShaderiv( shader, GL_COMPILE_STATUS, &status );
+	if ( status == GL_FALSE ) {
+		GLint infoLogLength;
+		glGetShaderiv( shader, GL_INFO_LOG_LENGTH, &infoLogLength );
+
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetShaderInfoLog( shader, infoLogLength, NULL, strInfoLog );
+
+		const char* strShaderType = NULL;
+		switch ( eShaderType ) {
+			case GL_VERTEX_SHADER:
+				strShaderType = "vertex";
+				break;
+			case GL_GEOMETRY_SHADER:
+				strShaderType = "geometry";
+				break;
+			case GL_FRAGMENT_SHADER:
+				strShaderType = "fragment";
+				break;
+			default:
+				break;
+		}
+
+		fprintf( stderr, "Compile failure in %s shader:\n%s\n", strShaderType, strInfoLog );
+		delete[] strInfoLog;
+	}
+	return shader;
+}
+
+GLuint CreateProgram( const std::vector<GLuint>& shaderList ) {
+	GLuint program = glCreateProgram();
+	for ( auto& shader : shaderList ) {
+		glAttachShader( program, shader );
+	}
+	glLinkProgram( program );
+
+	GLint status;
+	glGetProgramiv( program, GL_LINK_STATUS, &status );
+	if ( status == GL_FALSE ) {
+		GLint infoLogLength;
+		glGetProgramiv( program, GL_INFO_LOG_LENGTH, &infoLogLength );
+		GLchar* strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog( program, infoLogLength, NULL, strInfoLog );
+		fprintf( stderr, "Linker failure: %s\n", strInfoLog );
+		delete[] strInfoLog;
+	}
+
+	for ( auto& shader : shaderList ) {
+		glDetachShader( program, shader );
+	}
+
+	return program;
+}
+
+GLuint theProgram;
+
+const std::string strVertexShader(
+	"#version 330\n"
+	"layout(location = 0) in vec4 position;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = position;\n"
+	"}\n"
+);
+
+const std::string strFragmentShader(
+	"#version 330\n"
+	"out vec4 outputColor;\n"
+	"void main()\n"
+	"{\n"
+	"	outputColor = vec4( 1.0f, 1.0f, 1.0f, 1.0f );\n"
+	"}\n"
+);
+
+void InitializeProgram() {
+	std::vector<GLuint> shaderList;
+	shaderList.push_back( CreateShader( GL_VERTEX_SHADER, strVertexShader ) );
+	shaderList.push_back( CreateShader( GL_FRAGMENT_SHADER, strFragmentShader ) );
+
+	theProgram = CreateProgram( shaderList );
+	
+	std::for_each( shaderList.begin(), shaderList.end(), glDeleteShader );
+}
+
+const float vertexPositions[] = {
+	0.75f, 0.75f, 0.0f, 1.0f,
+	0.75f, -0.75f, 0.0f, 1.0f,
+	-0.75f, -0.75f, 0.0f, 1.0f,
+};
+
+GLuint positionBufferObject;
+GLuint vao;
+
+void InitializeVertexBuffer() {
+	glGenBuffers( 1, &positionBufferObject );
+	glBindBuffer( GL_ARRAY_BUFFER, positionBufferObject );
+	glBufferData( GL_ARRAY_BUFFER, sizeof( vertexPositions), vertexPositions, GL_STATIC_DRAW );
+	glBindBuffer( GL_ARRAY_BUFFER, 0 );
+}
+
+void init() {
+	InitializeProgram();
+	InitializeVertexBuffer();
+
+	glGenVertexArrays( 1, &vao );
+	glBindVertexArray( vao );
+}
+
+void display() {
+	glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+	glClear( GL_COLOR_BUFFER_BIT );
+
+	glUseProgram( theProgram );
+
+	glBindBuffer( GL_ARRAY_BUFFER, positionBufferObject );
+	glEnableVertexAttribArray( 0 );
+	glVertexAttribPointer( 0, 4, GL_FLOAT, GL_FALSE, 0, 0 );
+
+	glDrawArrays( GL_TRIANGLES, 0, 3 );
+
+	glDisableVertexAttribArray( 0 );
+	glUseProgram( 0 );
+}
 
 static void error_callback( int error, const char* description ) {
 	std::cerr << description << ", code: " << error;
@@ -21,7 +158,7 @@ int main( int argc, char* argv[] ) {
 	if ( !glfwInit() ) {
 		exit( EXIT_FAILURE );
 	}
-	window = glfwCreateWindow( 640, 480, "Simple example", NULL, NULL );
+	window = glfwCreateWindow( 640, 480, "game-boilerplate", NULL, NULL );
 	if ( window == nullptr ) {
 	    glfwTerminate();
 		exit( EXIT_FAILURE );
@@ -29,30 +166,22 @@ int main( int argc, char* argv[] ) {
 	glfwMakeContextCurrent(window);
 	glfwSetKeyCallback(window, key_callback);
 
+	glewInit();
+	init();
+
 	while ( !glfwWindowShouldClose( window ) ) {
 		float ratio;
 		int width, height;
 		glfwGetFramebufferSize( window, &width, &height );
 		ratio = width / static_cast<float>( height );
 		glViewport( 0, 0, width, height );
-		glClear( GL_COLOR_BUFFER_BIT );
-		glMatrixMode( GL_PROJECTION );
-		glLoadIdentity();
-		glOrtho( -ratio, ratio, -1.f, 1.f, 1.f, -1.f );
-		glMatrixMode( GL_MODELVIEW );
-		glLoadIdentity();
-		glRotatef( (float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f );
-		glBegin( GL_TRIANGLES );
-		glColor3f( 1.f, 0.f, 0.f );
-		glVertex3f( -0.6f, -0.4f, 0.f );
-		glColor3f( 0.f, 1.f, 0.f );
-		glVertex3f( 0.6f, -0.4f, 0.f );
-		glColor3f( 0.f, 0.f, 1.f );
-		glVertex3f( 0.f, 0.6f, 0.f );
-		glEnd();
+		
+		display();
+		
 		glfwSwapBuffers( window );
 		glfwPollEvents();
 
+		// C++11 standard library test
 		std::this_thread::sleep_for( std::chrono::milliseconds( 1 ) );
 	}
 	glfwDestroyWindow( window );
